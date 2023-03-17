64c73c7174a6399a59a4fa7f5827f9fd2026d5e9
buteramyers <buteramyers96@gmail.com>
2023-03-17 16:38:58 -0700

    program that prints alphabet in both lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+	char B;
+
+	for (a = 'a' ; a <= 'z' ; ++a)
+	{
+		putchar(a);
+	}
+
+	for (B = 'A' ; B <= 'Z' ; ++B)
+	{
+		putchar(B);
+	}
+	putchar('\n');
+
+	return (0);
+}
