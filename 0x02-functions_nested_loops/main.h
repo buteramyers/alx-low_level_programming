8c3322f2e04176471456feab51a5788e1ea683b2
buteramyers <buteramyers96@gmail.com>
2023-03-21 16:50:49 -0700

    added a _putchar file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 /**
  * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 0 (success)
  */
-int _putchar(char c)
-
+int _putchar(char c);
 #endif
