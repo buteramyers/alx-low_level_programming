e41638ea4dd632fb4d896a43d20b61ae8415dbb0
buteramyers <buteramyers96@gmail.com>
2023-03-27 16:59:34 -0700

    function that prints every character of a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,14 @@ void rev_string(char *s)
 	int length, z, half;
 	char temp;
 
-	for
-	(length = 0; s[length] != '\0';)
+	for (length = 0;)
 	{
-		length++;
-		z = 0;
-		half = length / 2;
+		if (s[length] != '\0')
+		{
+			length++;
+			z = 0;
+			half = length / 2;
+		}
 	}
 
 	while (half--)
