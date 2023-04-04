55625bf80acc52248d59b2b85449f68efb85905d
buteramyers <buteramyers96@gmail.com>
2023-04-04 16:02:59 -0700

    deleeted duplicates

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _memset - function to fill memory
+ * @s: Pointer which holds the value
+ * @b: the constant
+ * @n: variable that will be filled with the constant
+ * Return: pointer *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int j;
+}
