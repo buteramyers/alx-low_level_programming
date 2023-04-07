08928ae1bcc2b24f4bd70d36aa2dd8b295e3a52a
buteramyers <buteramyers96@gmail.com>
2023-04-06 17:37:26 -0700

    changed the iterator to accept larger inputs

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,24 +2,27 @@
 
 /**
  * _sqrt_help - for checking if n has a natural square root
- * _sqrt_recursion - return the square root of the number
  * @i: factor that gives n when multiplied
  * @n: number to be processed
- * Return: 0
+ * Return: the value
  */
 int _sqrt_help(int n, int i)
 {
-	if (i == 0)
+	if (i * i > n)
 		return (-1);
 	else if (i * i == n)
 		return (i);
 	else
-		return (_sqrt_help(n, i - 1));
+		return (_sqrt_help(n, i + 1));
 }
-
+ /**
+  * _sqrt_recursion - get the square root of the number
+  * @n: number to be processed
+  * Return: _sqrt_help
+  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt_help(n, 35));
+	return (_sqrt_help(n, 0));
 }
