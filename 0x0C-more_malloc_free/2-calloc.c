19591397619d11266bd565c718546d281ea1e60a
buteramyers <buteramyers96@gmail.com>
2023-04-14 07:17:20 -0700

    calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,14 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *s;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
+	s = calloc(nmemb, size);
+	if (s == NULL)
+		return (NULL);
+
+	return (s);
 }
