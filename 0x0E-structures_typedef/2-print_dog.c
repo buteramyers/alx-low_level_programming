e0251c7163c5fc96c19688c1c221d408dc45ed5b
buteramyers <buteramyers96@gmail.com>
2023-04-18 07:16:08 -0700

    print the struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - print the dog
+ * @d: pointer to the instantiation of the dog
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("nil");
+		exit(1);
+	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
