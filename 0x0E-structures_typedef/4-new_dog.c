5c780f2b490e1a7bca78e3d8a875a7e578107700
buteramyers <buteramyers96@gmail.com>
2023-04-18 13:11:34 -0700

    freed all the memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,17 +17,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 	{
-		free(new_dog);
-		return (NULL);
+		return(NULL);
 	}
 	new_dog->name = malloc(strlen(name) + 1);
 	new_dog->owner = malloc(strlen(owner) + 1);
 
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	if (new_dog->name != NULL)
 	{
 		free(new_dog->name);
+		free(new_dog);
+	}
+	if (new_dog->owner != NULL)
+	{
 		free(new_dog->owner);
-		return (NULL);
+		free(new_dog);
 	}
 	strcpy(new_dog->name, name);
 	new_dog->name[strlen(name)] = '\0';
