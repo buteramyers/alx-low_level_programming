78c0ecb33a8ea7c57e1a43a7b30fddd18a11074a
buteramyers <buteramyers96@gmail.com>
2023-04-18 16:43:52 -0700

    new file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,10 +16,12 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
+		d->name = "(nil)";
 		printf("Name: nil\n");
 	}
 	if (d->owner == NULL)
 	{
+		d->owner = "(nil)";
 		printf("Owner: nil\n");
 	}
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
