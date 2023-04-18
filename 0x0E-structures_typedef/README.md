9d242be13705c000800a69f58d45ea676b6fb2a2
buteramyers <buteramyers96@gmail.com>
2023-04-18 06:12:15 -0700

    template for a dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This project covers content about structures
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - template for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: void
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
