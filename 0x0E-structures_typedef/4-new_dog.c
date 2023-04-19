f0012c29482777353f2f95368a301f653aebc68b
buteramyers <buteramyers96@gmail.com>
2023-04-18 18:19:03 -0700

    new file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,29 +12,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	int name_len, owner_len;
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-	{
 		return (NULL);
-	}
-	new_dog->name = strdup(name);
+	name_len = strlen(name);
+	owner_len = strlen(owner);
 
-
+	new_dog->name = malloc(sizeof(char) * (name_len + 1));
+	new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
-	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog->name);
 		free(new_dog);
+		free(new_dog->name);
 		return (NULL);
 	}
+
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
+
 	new_dog->age = age;
 
 	return (new_dog);
