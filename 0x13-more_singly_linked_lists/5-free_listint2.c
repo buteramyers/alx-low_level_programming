0132c7529e909844efd4327ecccceb9f56c47d5b
buteramyers <buteramyers96@gmail.com>
2023-04-30 18:59:26 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the unused bytes
+ * @head: location of the current node
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
